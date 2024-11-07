986e8e0701f6910014001c1bea5d841717b22054
Likhon <likh.deshi@gmail.com>
2024-11-07 18:04:14 +0600

    Validate using checkpoint

--- main.py
+++ main.py
@@ -16,36 +16,24 @@ def run() -> None:
     # Get the Great Expectations context
     context = gx.get_context(mode="file", project_root_dir="./gx")
 
-    # Define validation definitions
-    validation_definitions = {
-        "distribution": context.validation_definitions.get("distribution"),
-        "missingness": context.validation_definitions.get("missingness"),
-        "schema": context.validation_definitions.get("schema"),
-        "volume": context.validation_definitions.get("volume"),
-    }
-
-    # Initialize an empty dictionary to hold validation results
-    validation_results = {}
-
-    # Define a unique run_id based on the current timestamp
     run_id = RunIdentifier(run_name="Quality", run_time=datetime.now(tz=timezone.utc).strftime('%Y%m%dT%H%M%S.%f'))
 
-    # Run validations for each definition
-    for name, validation_definition in validation_definitions.items():
-        if validation_definition is not None:
-            # Run the Validation Definition
-            result = validation_definition.run(batch_parameters={"dataframe": df}, run_id=run_id)
-            validation_results[name] = result
-        else:
-            logging.error(f"Validation definition '{name}' not found.")
+    statistical_result = context.checkpoints.get("statistical_checkpoint").run(
+        batch_parameters={"dataframe": df}, run_id=run_id,
+    )
 
-    # Review the Validation Results
-    logging.info(validation_results)
+    completeness_result = context.checkpoints.get("completeness_checkpoint").run(
+        batch_parameters={"dataframe": df}, run_id=run_id,
+    )
+
+    logging.info(f"Validation results: {statistical_result.success and completeness_result.success}")
 
     # Build the Data Docs
     context.build_data_docs()
     # Print out the docs URL
     logging.info(f"Data Docs available at: {context.get_docs_sites_urls()[0]['site_url']}")
+
+    # todo: Add actions to checkpoints to trigger notifications and alerts
 
 if __name__ == '__main__':
     run()
